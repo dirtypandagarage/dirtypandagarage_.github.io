4ac46af9157c6368eb3c5885e5754baa27f2520b
Milou <tutkutuncer03@gmail.com>
2021-05-17 17:26:16 +0300

    second commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const todos = [{
+        id: 1,
+        text: "take out the trash",
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: "meeting with boss",
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: "dentist appt",
+        isCompleted: false
+    }
+
+]
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
